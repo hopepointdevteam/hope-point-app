8cdf156e2d555613495d02e80ae301f74bb17280
Scott Capps <capps.scott@gmail.com>
2018-08-30 18:54:39 -0700

    footer worked on

--- Desktop/HopePoint/hopepoint-app/src/app/constants/staff-members.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/staff-members.ts
@@ -2,10 +2,24 @@ import { Staff } from '../models/staff';
 
 export const StaffMembers: Staff[] = [
   {
-    name: string, 
-    ImgUrl: string,
-    title: string,
-    facebook: string,
-    twitter: string
-  }  
+    name: 'Doug Smith',  
+    ImgUrl: '../../assets/images/staff/DougSmith.jpg',
+    title: 'Lead Pastor',
+    facebook: 'facebook.com/doug',
+    twitter: '@doug'
+  },
+  {
+    name: 'Dan Hasting',  
+    ImgUrl: '../../assets/images/staff/DanHastings.jpg',
+    title: 'Associate Pastor',
+    facebook: 'facebook.com/dan',
+    twitter: '@dan'
+  },
+  {
+    name: 'Charvis Franklin',  
+    ImgUrl: '../../assets/images/staff/CharvisFranklin.jpg',
+    title: 'Youth Pastor',
+    facebook: 'facebook.com/charvis',
+    twitter: '@charvis'
+  } 
 ]
