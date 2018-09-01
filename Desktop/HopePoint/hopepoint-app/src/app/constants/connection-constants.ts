29dbf92cb6e55554faf3768683917252f3a4e1be
Scott Capps <capps.scott@gmail.com>
2018-08-31 21:16:39 -0700

    current build before deploy

--- Desktop/HopePoint/hopepoint-app/src/app/constants/connection-constants.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/connection-constants.ts
@@ -4,25 +4,25 @@ export const HPConnections: Connections[] = [
   {
     id: 0,
     name: 'I am Hope Point', 
-    ImgUrl: '../../../assets/images/IAmHopePoint_BG.jpg', 
+    ImgUrl: '/assets/images/IAmHopePoint_BG.jpg', 
     description: 'Are you interested in knowing more about Hope Point? Are you wanting to become a member? Take our monthly class to become a member so you can answer the question, “Why Hope Point?”.	You are also welcome to join us for lunch to meet Pastor Doug and the Hope Point Staff. Hear the vision of our church, and how you can connect.'
   },
   {
     id: 1,
     name: 'Baptisms', 
-    ImgUrl: '../../../assets/images/Baptism_BG.jpg', 
+    ImgUrl: '/assets/images/Baptism_BG.jpg', 
     description: 'An outward expression of a decision to follow Jesus Christ as savior and Lord of your life. To learn more or to schedule your baptism please email us.'
   },
   {
     id: 2,
     name: 'Sit One, Serve One', 
-    ImgUrl: '../../../assets/images/SitOneServeOne_BG.jpg', 
+    ImgUrl: '/assets/images/SitOneServeOne_BG.jpg', 
     description: 'Want to connect to people? Volunteer! Sit one service and serve the other. We have many opportunities on Sunday morning that we would love to see you take advantage of!'
   },
   {
     id: 3,
     name: 'Community', 
-    ImgUrl: '../../../assets/images/Community_BG.jpg', 
+    ImgUrl: '/assets/images/Community_BG.jpg', 
     description: 'At 10am every 3rd Thursday of the month we take coffee and breakfast to our friends at the Riverbottoms. We spend talking about life and the love of Jesus. The bike kitchen was started to help our homeless community. Every 3rd Thursday a group of volunteers takes bike locks, parts and tools to the riverbottoms and set up our own bike repair shop. It’s a way to not only help our homeless community but build relationships with some amazing people.'
   }
 ]
--- Desktop/HopePoint/hopepoint-app/src/app/constants/worship-leaders.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/worship-leaders.ts
@@ -3,14 +3,14 @@ import { Staff } from '../models/staff';
 export const WorshipLeaders: Staff[] = [
   {
     name: 'John Waller',
-    ImgUrl: '../../assets/images/staff/JohnWaller.jpg',
+    ImgUrl: '/assets/images/staff/JohnWaller.jpg',
     title: 'Worship Leader',
     facebook: 'facebook.com/john',
     twitter: '@john'
   },
   {
     name: 'Collin Humphrey',
-    ImgUrl: '../../assets/images/staff/CollinHumphrey.jpg',
+    ImgUrl: '/assets/images/staff/CollinHumphrey.jpg',
     title: 'Worship Leader',
     facebook: 'facebook.com/collin',
     twitter: '@collin'
