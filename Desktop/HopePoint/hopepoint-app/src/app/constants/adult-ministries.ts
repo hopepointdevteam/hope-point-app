03e3bcb0a25ff5e9f1c2f59d28a64d2e38b2ca3f
Scott Capps <capps.scott@gmail.com>
2018-08-31 23:26:21 -0700

    preview build

--- Desktop/HopePoint/hopepoint-app/src/app/constants/adult-ministries.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/adult-ministries.ts
@@ -1,7 +1,7 @@
 import { Ministry } from '../models/ministries';
 
 export const AdultMinistries: Ministry[] = [
-  {id: 0, name: "Hope Point Men", href: "mens", ImgUrl: "/preview/assets/images/Mens_BG.jpg",logoUrl:"/preview/assets/images/Mens_Logo_350x350.png", MinistryGroup: 'Adult'},
-  {id: 1, name: "Hope Point Ladies", href: "ladies", ImgUrl: "/preview/assets/images/Ladies_BG.jpg", logoUrl: "/preview/assets/images/Ladies_Logo_350x350.png", MinistryGroup: 'Adult'},
-  {id: 2, name: "PT Cruisers", href: "cruisers", ImgUrl: "/preview/assets/images/Senior_BG.jpg", logoUrl: "/preview/assets/images/Senior Adult Logo.png", MinistryGroup: 'Adult'}
+  {id: 0, name: "Hope Point Men", href: "mens", ImgUrl: "/preview/assets/images/backgrounds/Mens_BG.jpg",logoUrl:"/preview/assets/images/logos/Mens_Logo_350x350.png", MinistryGroup: 'Adult'},
+  {id: 1, name: "Hope Point Ladies", href: "ladies", ImgUrl: "/preview/assets/images/backgrounds/logos/Ladies_BG.jpg", logoUrl: "/preview/assets/images/Ladies_Logo_350x350.png", MinistryGroup: 'Adult'},
+  {id: 2, name: "PT Cruisers", href: "cruisers", ImgUrl: "/preview/assets/images/backgrounds/Senior_BG.jpg", logoUrl: "/preview/assets/images/logos/Senior Adult Logo.png", MinistryGroup: 'Adult'}
 ]
--- Desktop/HopePoint/hopepoint-app/src/app/constants/all-ministries.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/all-ministries.ts
@@ -1,11 +1,11 @@
 import { Ministry } from '../models/ministries';
 
 export const AllMinistries: Ministry[] = [
-  {id: 0, name: "Hope Point Men", href: "ministries/mens", ImgUrl: "/preview/assets/images/Mens_BG.jpg", logoUrl:"/preview/assets/images/Mens_Logo_350x350.png", MinistryGroup: 'Adult'},
-  {id: 1, name: "Hope Point Ladies", href: "ministries/ladies", ImgUrl: "/preview/assets/images/Ladies_BG.jpg", logoUrl: "/preview/assets/images/Ladies_Logo_350x350.png", MinistryGroup: 'Adult'},
-  {id: 2, name: "PT Cruisers", href: "ministries/cruisers", ImgUrl: "/preview/assets/images/Senior_BG.jpg", logoUrl: "/preview/assets/images/Senior Adult Logo.png", MinistryGroup: 'Adult'},
-  {id: 3, name: "Kids", href: "ministries/kids", ImgUrl: "/preview/assets/images/HPKids_BG.jpg", logoUrl: "/preview/assets/images/Kids_Logo_350x350.png", MinistryGroup: 'Family'},
-  {id: 4, name: "Club 45", href: "ministries/club45", ImgUrl: "/preview/assets/images/Club45_BG.jpg", logoUrl: "/preview/assets/images/Club45_Logo_350x350.png", MinistryGroup: 'Family'},
-  {id: 5, name: "Jr. High", href: "ministries/jrhigh", ImgUrl: "/preview/assets/images/JrHigh_BG.jpg", logoUrl: "/preview/assets/images/YF_JrHigh_logo_350x350.png", MinistryGroup: 'Family'},
-  {id: 6, name: "High School", href: "ministries/highshcool", ImgUrl: "/preview/assets/images/hs.jpg", logoUrl: "/preview/assets/images/YF_High_logo_350x350.png", MinistryGroup: 'Family' }
+  {id: 0, name: "Hope Point Men", href: "ministries/mens", ImgUrl: "/preview/assets/images/backgrounds/Mens_BG.jpg", logoUrl:"/preview/assets/images/logos/Mens_Logo_350x350.png", MinistryGroup: 'Adult'},
+  {id: 1, name: "Hope Point Ladies", href: "ministries/ladies", ImgUrl: "/preview/assets/images/backgrounds/Ladies_BG.jpg", logoUrl: "/preview/assets/images/logos/Ladies_Logo_350x350.png", MinistryGroup: 'Adult'},
+  {id: 2, name: "PT Cruisers", href: "ministries/cruisers", ImgUrl: "/preview/assets/images/backgrounds/Senior_BG.jpg", logoUrl: "/preview/assets/images/logos/Senior Adult Logo.png", MinistryGroup: 'Adult'},
+  {id: 3, name: "Kids", href: "ministries/kids", ImgUrl: "/preview/assets/images/backgrounds/HPKids_BG.jpg", logoUrl: "/preview/assets/images/logos/Kids_Logo_350x350.png", MinistryGroup: 'Family'},
+  {id: 4, name: "Club 45", href: "ministries/club45", ImgUrl: "/preview/assets/images/backgrounds/Club45_BG.jpg", logoUrl: "/preview/assets/images/logos/Club45_Logo_350x350.png", MinistryGroup: 'Family'},
+  {id: 5, name: "Jr. High", href: "ministries/jrhigh", ImgUrl: "/preview/assets/images/backgrounds/JrHigh_BG.jpg", logoUrl: "/preview/assets/images/logos/YF_JrHigh_logo_350x350.png", MinistryGroup: 'Family'},
+  {id: 6, name: "High School", href: "ministries/highshcool", ImgUrl: "/preview/assets/backgrounds/images/hs.jpg", logoUrl: "/preview/assets/images/logos/YF_High_logo_350x350.png", MinistryGroup: 'Family' }
 ]
