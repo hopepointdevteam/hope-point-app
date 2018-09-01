20fbea1256687b6e733e365f56c495f8bd5d8fec
Scott Capps <capps.scott@gmail.com>
2018-08-31 21:19:03 -0700

    preview branch

--- Desktop/HopePoint/hopepoint-app/src/app/constants/staff-members.ts
+++ Desktop/HopePoint/hopepoint-app/src/app/constants/staff-members.ts
@@ -3,21 +3,21 @@ import { Staff } from '../models/staff';
 export const StaffMembers: Staff[] = [
   {
     name: 'Doug Smith',  
-    ImgUrl: '/assets/images/staff/DougSmith.jpg',
+    ImgUrl: '/preview/assets/images/staff/DougSmith.jpg',
     title: 'Lead Pastor',
     facebook: 'facebook.com/doug',
     twitter: '@doug'
   },
   {
     name: 'Dan Hasting',  
-    ImgUrl: '/assets/images/staff/DanHastings.jpg',
+    ImgUrl: '/preview/assets/images/staff/DanHastings.jpg',
     title: 'Associate Pastor',
     facebook: 'facebook.com/dan',
     twitter: '@dan'
   },
   {
     name: 'Charvis Franklin',  
-    ImgUrl: '/assets/images/staff/CharvisFranklin.jpg',
+    ImgUrl: '/preview/assets/images/staff/CharvisFranklin.jpg',
     title: 'Youth Pastor',
     facebook: 'facebook.com/charvis',
     twitter: '@charvis'
